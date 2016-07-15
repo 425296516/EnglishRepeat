e0a5e646f328f571303514ffd4d09b048c52b1d5
zhangcirui <425296516@qq.com>
2016-07-15 15:42:17 +0800

    test

--- app/src/main/java/com/baidu/android/voicedemo/ActivityMain.java
+++ app/src/main/java/com/baidu/android/voicedemo/ActivityMain.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
+
 import com.baidu.speech.recognizerdemo.R;
 
 /**
@@ -18,6 +20,7 @@ public class ActivityMain extends PreferenceActivity {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         sp.edit().remove(Constant.EXTRA_INFILE).commit(); // infile参数用于控制识别一个PCM音频流（或文件），每次进入程序都将该值清楚，以避免体验时没有使用录音的问题
 
+        Toast.makeText(ActivityMain.this, "提交", Toast.LENGTH_SHORT).show();
         //tijiao
     }
 }
